3c6b09cde37f1def7946d492babc722bb8eb9434
Cap <caption187@gmail.com>
2023-01-06 00:19:49 +0800

    商品功能 - 查詢商品列表(基礎實作)

--- springboot-mall/src/main/java/com/capliao/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/capliao/springbootmall/service/ProductService.java
@@ -4,9 +4,14 @@ import com.capliao.springbootmall.dao.ProductDao;
 import com.capliao.springbootmall.dto.ProductRequest;
 import com.capliao.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
 
+    List<Product> getProducts();
+
     Product getProductById(Integer productId);
+
     Integer createProduct(ProductRequest productRequest);
 
     void updateProduct(Integer productId , ProductRequest productRequest);
